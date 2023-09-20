f73864f4b44ffe09b031107f4fff8da4ec319429
coatespeter <65430782+coatespeter@users.noreply.github.com>
2023-09-20 10:10:22 +0200

    add some content for modal box and game description. working on js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
 
 
+const chosenDoor; // NOW PAPER
+
+ chooseDoor();
+ runThroughDoor(chosenDoor)
+
+function chooseDoor() {
+  const doors = ['paper', 'wood', 'wood'];
+  const chosenDoorIndex = Math.floor(Math.random() * doors.length);
+  chosenDoor = doors[chosenDoorIndex];
+}
+
+function runThroughDoor(chosenDoor){
+  if (chosenDoor === 'paper') {
+    console.log("You made it to the next set of doors!");
+    // Here you can proceed to the next level or perform some other action
+  } else {
+    console.log("You hit the hard door and lose.");
+    // Here you can handle what happens when the player loses
+  }
+}
