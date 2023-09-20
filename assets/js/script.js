58d3f75da19824e649634be814f1c0d6f5069740
coatespeter <65430782+coatespeter@users.noreply.github.com>
2023-09-20 12:23:08 +0200

    add music file and functionality on button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,21 @@
 let modal = document.querySelector(".modal");
 let modalButton = document.getElementById("modal-btn");
 let closeButton = document.querySelector(".close-button");
-let resetButton = document.getElementById("reset-button");
+let resetButton = document.getElementById("reset-btn");
+const musicButton = document.getElementById('music-btn');
+
+//music controls
+
+musicButton.addEventListener("click", () => {
+  if (audio.paused) {
+      audio.volume = 0.2;
+      audio.play();
+      musicButton.innerHTML = "Stop Music";
+  } else {
+      audio.pause();
+      musicButton.innerHTML = "Play Music";
+  }
+});
 
 //modal box settings
 
