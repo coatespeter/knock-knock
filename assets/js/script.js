7d788c8d92f3f4e6f5753fa92ee0e2f63ad50dc6
coatespeter <65430782+coatespeter@users.noreply.github.com>
2023-10-02 12:00:29 +0200

    js variable update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 /* jshint esversion: 11 */
 let playerDirection;
 let paperDoorPosition;
-
 let modal = document.querySelector(".modal");
 let modalButton = document.getElementById("modal-button");
 let closeButton = document.querySelector(".close-button");
 let resetButton = document.getElementById("reset-button");
-
 const audio = document.getElementById("audio");
 const musicButton = document.getElementById("music-button");
 const controlButtons = document.querySelectorAll(".control-button");
